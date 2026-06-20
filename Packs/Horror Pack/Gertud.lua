52f0f3b621f8ababdef11497f0ebefa6b0baaa17
arkanzulfadliputra-oss <arkanzulfadliputra@gmail.com>
2026-06-20 18:21:35 +0800

    Refactor loading messages and game links

--- Packs/Horror Pack/Gertud.lua
+++ Packs/Horror Pack/Gertud.lua
@@ -44,9 +44,7 @@ SubLoading.TextSize = 16
 
 task.spawn(function()
     for i = 0,100 do
-
         Loading.Text = "Loading "..i.."/100"
-
         if i <= 20 then
             SubLoading.Text = "Credit By ScriptGaming"
         elseif i <= 40 then
@@ -58,7 +56,6 @@ task.spawn(function()
         else
             SubLoading.Text = "Loaded!"
         end
-
         task.wait(0.03)
     end
 end)
@@ -66,72 +63,53 @@ end)
 task.wait(3.5)
 
 local Games = {
-
     [6205205961] = "https://pastebin.com/raw/aXakLQws",
     [1985320156] = "https://pastebin.com/raw/4ZpyA95n",
     [4480809144] = "https://pastebin.com/raw/J6KezjYT",
     [116387287343653] = "https://pastebin.com/raw/b9aw5sN9",
     [14476003462] = "https://pastebin.com/raw/VuBy59XD",
     [137811364710617] = "https://pastebin.com/raw/muNr8497",
-
     [9120716669] = "https://raw.githubusercontent.com/arkanzulfadliputra-oss/Games/main/Insane%20Elevator%20Testing/Script.lua",
-
     [16089229671] = "https://raw.githubusercontent.com/arkanzulfadliputra-oss/Games/main/Survival%20Granny/Script.lua",
-
     [5777099015] = "https://raw.githubusercontent.com/arkanzulfadliputra-oss/Games/main/Cheese%20Horror/Chapter%201/Script.lua",
-
     [9053673709] = "https://raw.githubusercontent.com/arkanzulfadliputra-oss/Games/main/Cheese%20Horror/Chapter%202/Script.lua",
-
     [6153766069] = "https://raw.githubusercontent.com/arkanzulfadliputra-oss/Games/main/Escape%20Carnival/Script.lua",
-
     [10384852727] = "https://pastebin.com/raw/VZp4HdVd",
-
-    [4785497374] = "https://raw.githubusercontent.com/arkanzulfadliputra-oss/Games/main/Granny/Granny%202/Script.lua"
+    [4785497374] = "https://raw.githubusercontent.com/arkanzulfadliputra-oss/Games/main/Granny/Granny%202/Script.lua",
+    [10384858885] = "https://raw.githubusercontent.com/arkanzulfadliputra-oss/Games/refs/heads/main/Granny/Granny%203/Script.lua"
 }
 
 local Link = Games[game.PlaceId]
 
 if Link then
-
     local success, response = pcall(function()
         return game:HttpGet(Link)
     end)
-
     if success then
-
         StarterGui:SetCore("SendNotification", {
             Title = "Success",
             Text = "Game Supported",
             Duration = 3
         })
-
         loadstring(response)()
-
     else
-
         Loading.Text = "Failed"
         SubLoading.Text = "HTTP Error"
-
         StarterGui:SetCore("SendNotification", {
             Title = "Error",
             Text = tostring(response),
             Duration = 5
         })
-
         task.wait(3)
     end
-
 else
-
     Loading.Text = "Failed"
     SubLoading.Text = "Wrong Game"
-
     StarterGui:SetCore("SendNotification", {
         Title = "Failed",
         Text = "Please play the game correctly",
         Duration = 5
     })
-
     task.wait(3)
 end
 
